baf908e52811ff9e19c4ee9d7f4fc50b1b8a46dc
Justin Joseph <justin.joseph0@yahoo.com>
2019-03-10 16:03:12 -0400

    Minor improvements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { AuthService } from './shared/services/auth.service';
+
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(private _auth: AuthService, private _router: Router) {}
+
+  ngOnInit(): void {
+    if ( this._auth.tokenValid ) this._router.navigate(['/']);
+  }
+}
--- src/app/core/validators/password-validators.ts
+++ src/app/core/validators/password-validators.ts
@@ -1,12 +1,12 @@
 import { FormGroup, ValidationErrors } from '@angular/forms';
 
 export class PasswordValidators {
-    static mustMatch(form: FormGroup): ValidationErrors | null {
-      const password = form.get('password').value;
-      const passwordConfirmation = form.get('passwordConfirmation').value;
+  static mustMatch(form: FormGroup): ValidationErrors | null {
+    const password = form.get('password').value;
+    const passwordConfirmation = form.get('passwordConfirmation').value;
 
-      return password === passwordConfirmation
-        ? null
-        : { mismatchedPasswords: true };
-    }
+    return password === passwordConfirmation
+      ? null
+      : { mismatchedPasswords: true };
+  }
 }
